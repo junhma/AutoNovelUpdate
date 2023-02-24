91d75d88f86f69deb6f5b1de8f86ba37908029f8
sera <66292094+forestmist-stack@users.noreply.github.com>
2023-02-23 17:21:43 -0800

    fixed path

--- __main__.py
+++ __main__.py
@@ -1,11 +1,10 @@
-from pathlib import Path
+from pathlib import Path, PurePath
 import autoupdate.edit as edit
 
-
-BASE_FOLDER = Path(__file__).parent.resolve()
+BASE_FOLDER = PurePath(Path.home(), "Documents/GitHub/autoupdate")
 BASE_NAME = r"reading lists - 小説.csv"
 #BASE_NAME = r"reading_lists_debug.csv"
 
-file = Path(BASE_FOLDER, BASE_NAME)
+file = PurePath(BASE_FOLDER, BASE_NAME)
 
 edit.convert(file, BASE_FOLDER)
